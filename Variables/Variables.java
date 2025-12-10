eeb0fcdd0c3aae24173cb2e886c5fef57130ce12
SumitGujariya <gujariyasumit96@gmail.com>
2025-12-10 17:16:31 +0530

    Initial commit - add project files

---
+++ Variables/Variables.java
@@ -0,0 +1,8 @@
+public class Variables {
+    public static void main(String[] args) {
+        int a = 45;
+        int b = 55;
+        int result = a + b;
+        System.out.println(result);
+    }
+}
