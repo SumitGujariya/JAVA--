cd7820d8041f678f6ef9f19bcaeb373348c6197d
SumitGujariya <gujariyasumit96@gmail.com>
2025-12-11 18:52:37 +0530

    add those files in repo

---
+++ DataTypes/Literals.java
@@ -0,0 +1,18 @@
+// Literals In Java.
+// They are that type of value you treat as a integer.
+
+public class Literals {
+    public static void main(String[] args) {
+        int num1 = 0b101; // This binary form to check the literals in java.
+        int num2 = 0x7e;  // This is Hexadecimal number in java.
+        int num3 = 10_00_00_000;  // In java we have feature to write a big value like this to count easily.
+        double num4 = 56; //  In java you assign a interger value in doubl it will converted the value in compile time.
+        char num5 = 'c'; //  In java character aslo treated as a integer 
+        
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(num5);
+    }
+}
---
+++ DataTypes/TpyeConversionAndCasting.java
@@ -0,0 +1,72 @@
+// Type Conversion and casting.
+/*
+Casting
+We have (Explicit) is called (Casting) in this method we change the value according to the our task.
+We have also (Implicit) is called (Conversion) in this method the compiler change the value according to the our task.
+We have also (Type Promotion) In this method we will promote the value with the help of they will increase their range.
+ 
+*/
+
+
+public class TpyeConversionAndCasting {
+    public static void main(String[] args) {
+
+        // This is for int to byte & byte to int.
+            
+        byte bt = 127; // This method is conversion (say automatic conversion with the help of compiler called ({IMPLICIT})).
+        int it = bt;
+        // bt = it; This wrong method to conversion this. It will throw you error.
+        // Output:- 127
+
+        System.out.println(it);
+
+        int a = 257;  // This method is casting (In method we convert the value our self to perform a task called ({EXPLICIT})).
+        byte b = (byte) a; //  It will convert the value with the help of (MODULO %) than it will show the value with the final value to the byte value.
+        // Output:- 1
+
+        System.out.println(b);
+
+    }
+}
+
+
+
+
+
+//            ***********     Float    ***********
+/* 
+ If you try to convert the value into it. you loose some value.
+ you also conversion in the float but you loose some value in it.
+
+ {
+
+    float ft = 5.6f;
+    int in = (int) f; // If you try to convert the value into it. you loose some value.
+
+    int in = 58;
+    float ft = (float) in; // It will work on in both types.
+
+ }
+
+*/
+
+
+
+
+
+//         ***********     Type Promotion   **********
+
+/*
+Another method we will see here is called type promotion in this they will promote the value into another type.
+To increase there size of the value.
+
+ {
+
+    byte a = 10;
+    byte b = 30;
+
+ }
+     
+    int result = a * b; // The reason of using int if we perform the task there size will be going out of range. so here we use int type promotion with the help of we we convert the value into byte to int to increase the range.
+
+*/
