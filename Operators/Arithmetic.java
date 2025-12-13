a6d16384636193cf5d684ce5986e9a821a892b5b
SumitGujariya <gujariyasumit96@gmail.com>
2025-12-13 14:36:40 +0530

    update the code

--- Operators/Arithmetic.java
+++ Operators/Arithmetic.java
@@ -1,18 +1,31 @@
 public class Arithmetic {
     public static void main(String[] args) {
         int num1 = 99;
-        int num2 = 1;
+        // int num2 = 1;
+        // Types the way you perform additoin & substraction task.
+        //  num1 = num1 + 1;
+        // num += 1;
+        // num1 ++;  // This is called post increament.
+        // ++ num1;  // This is called pre increament.
+
+        int result1 = ++num1;  // Increament the value then fetch.
+        int result2 = num1++;  // Fetch the value then increament. 
+        System.out.println(result1);
+        //output:- 100
+        System.out.println(result2);
+        //output:- 99
+
         // int result = num1 + num2;
         // int result = num1 - num2;
         // int result = num1 * num2;
         // int result = num1 / num2;
         // int result = num1 % num2;
 
-        System.out.println(num1 + num2);
-        System.out.println(num1 - num2);
-        System.out.println(num1 * num2);
-        System.out.println(num2 / num1);
-        System.out.println(num1 % num2);
+        // System.out.println(num1 + num2);
+        // System.out.println(num1 - num2);
+        // System.out.println(num1 * num2);
+        // System.out.println(num2 / num1);
+        // System.out.println(num1 % num2);
         
     }
 }
