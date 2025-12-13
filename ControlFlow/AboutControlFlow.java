0de2b58b18240e972a1cd14b7fe7c3e0d0eacb49
SumitGujariya <gujariyasumit96@gmail.com>
2025-12-14 01:45:29 +0530

    update this newly added file

---
+++ ControlFlow/AboutControlFlow.java
@@ -0,0 +1,13 @@
+/*
+Control Flow:
+Control Flow Statements in Java are used to control the order in which statements are executed in a program. They decide which block of code runs and when.
+They are of two types, i.e.
+1. Decision making statements: Decision-making statements let your program make choices and execute different code depending on conditions.
+   if-else
+   switch
+
+2. Looping statements: Repeatedly execute a block of code based on a specified condition. These statements are essential for tasks requiring repetition.
+   while loop
+   do-while loop
+   for loop
+*/
