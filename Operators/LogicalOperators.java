bc670c202daf613a1ce2df063f8c4d03695b12bf
SumitGujariya <gujariyasumit96@gmail.com>
2025-12-14 00:53:09 +0530

    update the code

--- Operators/LogicalOperators.java
+++ Operators/LogicalOperators.java
@@ -3,8 +3,23 @@ public class LogicalOperators {
         int num1 = 4;
         int num2 = 5;
 
+        // You apply this with more than 2 condition also.
         System.out.println((num1 > num2) && (num1 < num2));
+        // In && logicla operators they work like this.
+        // T > T = T
+        // T > F = F
+        // F > T = F
+        // F > F = F
         System.out.println((num1 > num2) || (num1 < num2));
+        // In || logicla operators they work like this.
+        // F > F = F
+        // F > T = T
+        // T > F = T
+        // T > T = T
         System.out.println(!(num1 > num2));
+        // In java it works on convert the value you say. with the help of thsi opertors you 
+        // Like it convert (False to True and True to False).
+        // Like if you have True and you want print False, than you apply a (Not operation).
+        // Like if you have False and you want print True, than you apply a (Not operation).
     }
 }
