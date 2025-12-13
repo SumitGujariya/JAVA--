bd7c7d9f8b4aa94a354ebc1083caac60be05e786
SumitGujariya <gujariyasumit96@gmail.com>
2025-12-14 00:17:40 +0530

    update this

--- Operators/Arithmetic.java
+++ Operators/Arithmetic.java
@@ -8,12 +8,17 @@ public class Arithmetic {
         // num1 ++;  // This is called post increament.
         // ++ num1;  // This is called pre increament.
 
+        // num++;    //post increment
+		// ++num;    //pre increment
+		// num--;    //post decrement
+		// --num;	  //pre decrement
         int result1 = ++num1;  // Increament the value then fetch.
         int result2 = num1++;  // Fetch the value then increament. 
         System.out.println(result1);
         //output:- 100
         System.out.println(result2);
         //output:- 99
+        
 
         // int result = num1 + num2;
         // int result = num1 - num2;
