aedfb451a79cc8255849a0b01622c0f8d80c958d
SumitGujariya <gujariyasumit96@gmail.com>
2025-12-21 02:15:07 +0530

    push this also

--- ObjectOrientedProgramming/OOPs.java
+++ ObjectOrientedProgramming/OOPs.java
@@ -1,15 +1,9 @@
 package ObjectOrientedProgramming;
 
 
-
-
-
 // Main class: From where the execution of code starts.
 class OOPs {
     public static void main(String[] args) {
 
     }
 }
-
-
-public
