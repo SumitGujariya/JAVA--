325501b59d0e8a8aaaf445095f7e67735cb4a7b6
SumitGujariya <gujariyasumit96@gmail.com>
2025-12-23 00:19:07 +0530

    add the new files

---
+++ ObjectOrientedProgramming/4PillarsOfOops/Inheritance.java
@@ -0,0 +1,16 @@
+// package ObjectOrientedProgramming.4PillarsOfOops;
+
+
+class shape {
+    String colors;
+}
+
+class Triangle extends shape {
+
+}
+
+public class Inheritance {
+    public static void main(String[] args) {
+        Triangle t1 = new Triangle();
+    }
+}
--- ObjectOrientedProgramming/4PillarsOfOops/Polymorphism.java
+++ ObjectOrientedProgramming/4PillarsOfOops/Polymorphism.java
@@ -1,5 +1,34 @@
 // package ObjectOrientedProgramming.4PillarsOfOops;
 
-public class Polymorphism {
+class Pens {
+    String colors;
+    int price;
+    
+    
+    public void penInfo(String colors) {
+        System.out.println(colors);
+    }
+    public void penInfo(int price) {
+        System.out.println(price);
+    }
+    public void penInfo(String colors, int price) {
+        System.out.println(colors + " " + price);
+    }
     
 }
+
+
+public class Polymorphism {
+    public static void main(String[] args) {
+        Pens cr = new Pens();
+        cr.colors = "Blue";
+        cr.price = 5;
+
+        cr.penInfo(cr.colors);
+        cr.penInfo(cr.price);
+        cr.penInfo(cr.colors, cr.price);
+        
+    }
+}
+
+
