4501d3af347bbab50ad95a163d5ae748d7556a0b
SumitGujariya <gujariyasumit96@gmail.com>
2025-12-27 23:47:07 +0530

    update all the changes

--- StringInJava/WrappersInJava.java
+++ StringInJava/WrappersInJava.java
@@ -2,10 +2,10 @@ package StringInJava;
 public class WrappersInJava {
     public static void main(String[] args) {
         int num = 7;
-        Integer num1 = num; // this is (autoboxing) is when you stored premetive type into object automatically.
+        Integer num1 = num; // This is (autoboxing) is when you stored premetive type into object automatically.
         System.out.println(num1);
 
-        int num2 = num1;  // this is (auto-unboxing) is when you take out the value premetive value for the oject.
+        int num2 = num1;  // This is (auto-unboxing) is when you take out the value premetive value for the oject.
         System.out.println(num2);
 
         // Here's you converst the string value into int with the help of (parse int).
