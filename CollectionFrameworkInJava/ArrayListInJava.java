0dd0d044f7db57323e6b7a0337113a823e9f8fcc
SumitGujariya <gujariyasumit96@gmail.com>
2026-01-01 00:05:53 +0530

    update the files and some changes

---
+++ CollectionFrameworkInJava/ArrayListInJava.java
@@ -0,0 +1,23 @@
+package CollectionFrameworkInJava;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ArrayListInJava {
+    public static void main(String[] args) {
+        
+        ArrayList<Integer> myNumbers = new ArrayList<>();
+        myNumbers.add(4);
+        myNumbers.add(6);
+        myNumbers.add(3);
+        myNumbers.add(1);
+        myNumbers.add(9);
+
+        Collections.sort(myNumbers);
+
+        for (Integer i : myNumbers) {
+
+            System.out.println(i);
+        }
+    }
+}
