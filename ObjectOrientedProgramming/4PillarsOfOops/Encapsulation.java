9a8489e89e1e77b74cea2578d4a19828e144ecc5
SumitGujariya <gujariyasumit96@gmail.com>
2026-01-19 00:39:05 +0530

    update some changes

--- ObjectOrientedProgramming/4PillarsOfOops/Encapsulation.java
+++ ObjectOrientedProgramming/4PillarsOfOops/Encapsulation.java
@@ -2,7 +2,7 @@
 Encapsulation is the process of combining data and functions into a single unit called class. 
 In Encapsulation, the data is not accessed directly; it is accessed through the functions present inside the class. 
 In simpler words, attributes of the class are kept private and public getter and setter methods are provided to manipulate these attributes. This encapsulation makes the concept of data.
-hiding possible.(Data hiding: a language feature to restrict access to members of an object, reducing the negative effect due to dependencies. e.g. "protected", "private" feature in Java).  
+Hiding possible.(Data hiding: a language feature to restrict access to members of an object, reducing the negative effect due to dependencies. e.g. "protected", "private" feature in Java).  
 */
 class Account {
     public String name; // This is public any one can access.
