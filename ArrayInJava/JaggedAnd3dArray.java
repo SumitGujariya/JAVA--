0d12437b464fbd2966fd7e215e5289aa083eedd6
SumitGujariya <gujariyasumit96@gmail.com>
2026-01-18 23:48:55 +0530

    update the changes

--- ArrayInJava/JaggedAnd3dArray.java
+++ ArrayInJava/JaggedAnd3dArray.java
@@ -1,13 +1,13 @@
 package ArrayInJava;
 public class JaggedAnd3dArray {
 	public static void main(String[] args) {
-		int nums[][] = new int[3][]; // jagged
+		int nums[][] = new int[3][]; // Jagged
 		nums[0] = new int[3]; // You describe the how many elements in those particular array. In this have (eelements).
 		nums[1] = new int[4]; // In this have (4 elements).
 		nums[2] = new int[2]; // In this have (2 elements).
 
 		// int nums[][]=new int [3][4];
-		// int nums[][]=new int [3][4][5]; // three dimensional.
+		// int nums[][]=new int [3][4][5]; // Three dimensional.
 
 		for (int i=0; i<nums.length; i++) {
 			for (int j=0; j<nums[i].length; j++) {
