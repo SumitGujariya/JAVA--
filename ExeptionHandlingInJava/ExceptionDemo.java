596ba64e2dae3d2e53bca7a41486b94301315477
SumitGujariya <gujariyasumit96@gmail.com>
2026-01-21 14:22:28 +0530

    upate the td's changes

--- ExeptionHandlingInJava/ExceptionDemo.java
+++ ExeptionHandlingInJava/ExceptionDemo.java
@@ -5,14 +5,14 @@
 public class ExceptionDemo { // Try to write the diffrent type of name to avoid the error not write directlu the name of (Exception).
   public static void main(String[ ] args) {
     int[] myNumbers = {1, 2, 3};
-    System.out.println(myNumbers[10]); // error!
+    System.out.println(myNumbers[10]); // Error!
 
     // It is good to try for critical condition.
     try {
       int nums[] = {1, 2, 3};
       System.out.println(nums[20]);
     } catch (Exception e) { // You can aslo specify the exception what type of execption is.
-      System.out.println("something went wrong"); 
+      System.out.println("Something went wrong"); 
     }
   }
 }
